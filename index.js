561cc7265c734be5632d48c4065e20da7df29f09
Alexander Savin <box.for.postman@gmail.com>
2013-03-17 00:47:50 +0400

    refactored and improved

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var PATH = require('path'),
-    DEFAULT_PORT = 8080,
-    DEFAULT_PATH = 'pages';
+    DEFAULT_PORT = 8080;
 
 require('coa').Cmd()
     .name(PATH.basename(process.argv[1]))
     .title(['File changes watcher for use with bem server.', '' +
-    'See https://github.com/apsavin/bem-watcher/ for more info.'].join('\n'))
+        'See https://github.com/apsavin/bem-watcher/ for more info.'].join('\n'))
     .helpful()
     .opt()
     .name('version').title('Show version')
@@ -20,14 +19,13 @@ require('coa').Cmd()
     .name('root').short('r').long('root')
     .title('project root (cwd by default)')
     .def(process.cwd())
-    .val(function (d) {
-        return PATH.resolve(d);
+    .val(function (r) {
+        return PATH.resolve(r);
     })
     .end()
     .opt()
     .name('directory').short('d').long('dir')
-    .title('directory to rebuild, default: ' + DEFAULT_PATH)
-    .def(DEFAULT_PATH)
+    .title('directory to rebuild, all bundles and pages dirs by default')
     .end()
     .opt()
     .name('port').short('p').long('port')
@@ -39,8 +37,13 @@ require('coa').Cmd()
     .title('hostname of bem server, default: any')
     .end()
     .act(function (opts) {
+        if (opts.directory) {
+            opts.directory = [PATH.join(opts.directory, '..', PATH.basename(opts.directory).replace(PATH.sep, ''))];
+        }
+        else {
+            opts.directory = [];
+        }
         require('bem').api.server(opts);
-        require('./rebuilder').setEnv(opts);
-        require('./watcher').watch(opts);
+        require('./lib/main').run(opts);
     })
     .run();
